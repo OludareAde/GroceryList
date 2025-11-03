9f065b1e3af86b5413dc5f10802744ec747aeb3e
infix123 <adediran.oludare@gmail.com>
2025-11-03 13:39:00 -0500

    New file created

---
+++ file/main.js
@@ -0,0 +1,19 @@
+async function saveFile() {
+  try {
+    // create a new handle
+    const newHandle = await window.showSaveFilePicker();
+
+    // create a FileSystemWritableFileStream to write to
+    const writableStream = await newHandle.createWritable();
+
+    // write our file
+    await writableStream.write("My first file content");
+    await writableStream.seek(70);
+    await writableStream.write("My second file content");
+
+    // close the file and write the contents to disk.
+    await writableStream.close();
+  } catch (err) {
+    console.error(err.name, err.message);
+  }
+}
